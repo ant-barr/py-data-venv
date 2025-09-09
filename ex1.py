2e949ddd4a2d7bec2b1cfcfd89bfa1491de83a54
ant-barr <barr.star77@gmail.com>
2025-09-09 14:00:51 -0500

    added vim motions

--- ex1.py
+++ ex1.py
@@ -2,10 +2,14 @@ import math
 
 x = 64
 
-answer = math.pow(math.cos(x), 2) + math.pow(math.sin(x), 2)
+answer = math.pow(math.cos(x), 2)
++ math.pow(math.sin(x), 2)
 print(answer)
+
 
 answer2 = math.cos(x)**2 + math.sin(x)**2
 print(answer2)
 
+y = x
+print(y)
 
